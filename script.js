ced59ab997aab0984cb02a3e66c0790721f61bdc
ZriouilAnas <120396174+ZriouilAnas@users.noreply.github.com>
2025-04-09 15:36:01 +0000

    add : effect

--- script.js
+++ script.js
@@ -3,23 +3,33 @@ const toogleStyle_Btn = document.getElementById("toogleStyle_Btn");
 
 let tgl = 2;
 const tglFunc = () => {
-  if (tgl == 2) {
-    styles.setAttribute("href", "styles2.css");
-    tgl = 3;
-    console.log(tgl);
-  } else if (tgl == 1) {
-    styles.setAttribute("href", "styles1.css");
-    tgl = 2;
-    console.log(tgl);
-  } else if (tgl == 3) {
-    styles.setAttribute("href", "styles3.css");
-    tgl = 4;
-    console.log(tgl);
-  } else if (tgl == 4) {
-    styles.setAttribute("href", "styles4.css");
-    tgl = 1;
-    console.log(tgl);
-  }
+  const overlay = document.getElementById("flash-overlay");
+
+  // 1. Show the flash
+  overlay.style.opacity = "1";
+
+  // 2. After a short delay, hide it and switch style
+  setTimeout(() => {
+    overlay.style.opacity = "0";
+
+    if (tgl == 2) {
+      styles.setAttribute("href", "styles2.css");
+      tgl = 3;
+      console.log(tgl);
+    } else if (tgl == 1) {
+      styles.setAttribute("href", "styles1.css");
+      tgl = 2;
+      console.log(tgl);
+    } else if (tgl == 3) {
+      styles.setAttribute("href", "styles3.css");
+      tgl = 4;
+      console.log(tgl);
+    } else if (tgl == 4) {
+      styles.setAttribute("href", "styles4.css");
+      tgl = 1;
+      console.log(tgl);
+    }
+  }, 150);
 };
 
 toogleStyle_Btn.addEventListener("click", tglFunc);
