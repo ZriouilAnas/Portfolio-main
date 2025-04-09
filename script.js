c3bbb6395814026d4a9d7367ccff5bce7eaebece
ZriouilAnas <120396174+ZriouilAnas@users.noreply.github.com>
2025-04-09 15:16:08 +0000

    add : styles

---
+++ script.js
@@ -0,0 +1,32 @@
+const styles = document.getElementById("styles");
+const toogleStyle_Btn = document.getElementById("toogleStyle_Btn");
+
+let tgl = 1;
+const tglFunc = () => {
+  if (tgl == 2) {
+    styles.setAttribute("href", "styles2.css");
+    tgl = 3;
+    console.log(tgl);
+  } else if (tgl == 1) {
+    styles.setAttribute("href", "styles1.css");
+    tgl = 2;
+    console.log(tgl);
+  } else if (tgl == 3) {
+    styles.setAttribute("href", "styles3.css");
+    tgl = 4;
+    console.log(tgl);
+  } else if (tgl == 4) {
+    styles.setAttribute("href", "styles4.css");
+    tgl = 1;
+    console.log(tgl);
+  }
+};
+
+toogleStyle_Btn.addEventListener("click", tglFunc);
+
+const toggleBtn = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggleBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
